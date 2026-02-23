0e629a42c42a0e5a8620fca676068850aa151dfe
crystalforceix <crystalforceix@gmail.com>
2026-02-23 18:36:53 +0700

    Add makeskel

---
+++ makeskel.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Init
+mkdir -p ./skel/.config/
+mkdir -p ./skel/Pictures/Wallpapers/
+
+# Variable
+HOME_DIR="./skel"
+HOME_CONFIG_DIR="./skel/.config/"
+HOME_WALLPAPERS_DIR="./skel/Pictures/Wallpapers/"
+
+# Setup
+# List of files and directories to move
+HOME_FILES=(
+  "./Home-Overlay/.icons"  
+)
+
+CONFIG_FILES=(
+  "./Home-Overlay/fish"
+  "./Home-Overlay/fuzzel"
+  "./Home-Overlay/gtk-3.0"
+  "./Home-Overlay/gtk-4.0"
+  "./Home-Overlay/helix"
+  "./Home-Overlay/kitty"
+  "./Home-Overlay/matugen"
+  "./Home-Overlay/niri"
+  "./Home-Overlay/swaync"
+  "./Home-Overlay/waybar"
+  "./Home-Overlay/wlogout"
+  "./Home-Overlay/defaults/starship.toml"
+)
+
+# Loop for HOME_DIR
+for h in "${HOME_FILES[@]}"; do
+  # Check if file or directory exists
+  if [[ -e "$h" ]]; then
+    # Move the file/directory to destination
+    mv -- "$h" "$HOME_DIR/"
+    echo "Moved: $h"
+  else
+    echo "Error: '$h' does not exist" >&2
+  fi
+done
+
+# Loop for HOME_CONFIG_DIR
+for c in "${CONFIG_FILES[@]}"; do
+  # Check if file or directory exists
+  if [[ -e "$c" ]]; then
+    # Move the file/directory to destination
+    mv -- "$c" "$HOME_CONFIG_DIR/"
+    echo "Moved: $c"
+  else
+    echo "Error: '$c' does not exist" >&2
+  fi
+done
+
+# Setup wallpaper
+mv ./Home-Overlay/defaults/default-wallpaper.jpg $HOME_WALLPAPERS_DIR
